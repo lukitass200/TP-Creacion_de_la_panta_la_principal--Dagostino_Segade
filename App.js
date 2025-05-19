549a8cf2127d8773421dc85199ea99efa76ed569
Android Studio <devandroid@ort>
2025-05-19 14:08:02 -0300

    cambios imagen, pero no anda

--- App.js
+++ App.js
@@ -1,22 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { View, TextInput, TouchableOpacity, Text, StyleSheet, Image } from 'react-native';
-
+import img from "./assets/imagen.png"
 
 
 export default function App() {
   return (
     <View style={styles.container}>
+      
       <StatusBar style="light" backgroundColor="#a33ea1" />
 
       <View style={styles.header}>
         <Text style={styles.headerText}>Login App (Dagostino_Segade)"</Text>
       </View>
 
-      
-      <TextInput style={styles.input} placeholder="Usuario/Gmail" placeholderTextColor="#a85bbd" />
-      <TextInput style={styles.input} placeholder="Contraseña" placeholderTextColor="#a85bbd" />
-
+    
+      <Image source={img} />
+      <View style={{display:'flex' , justifyContent:'center', flexDirection:"column", backgroundColor: "blue", width: "100vw"}}>
+  
+        <TextInput style={styles.input} placeholder="Usuario/Gmail" placeholderTextColor="#a85bbd"  />
+        <TextInput style={styles.input} placeholder="Contraseña" placeholderTextColor="#a85bbd" />
+      </View>
    
     
       <TouchableOpacity style={styles.button}>
@@ -33,6 +37,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  
   },
   header: {
     height: 80,
@@ -50,14 +55,18 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   input: {
+
     borderWidth: 3,
     margintop:30,
-    paddingBlockStart:2,
+
     borderColor: '#a33ea1',
     borderRadius: 5,
-    width:350,
+    width:"50vw",
     marginVertical: 10,
     margin:5,
+    alignItems: "center",
+    justifyContent: "center"
+
   },
   button: {
     backgroundColor: '#c4b3e6',
